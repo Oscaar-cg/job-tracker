52afe653ebf4b6ace44b529d6c7809517cb9b76c
Oscaar-cg <oscar.canoa@outlook.fr>
2025-09-29 16:17:27 -0600

    add code in home & tracker page

---
+++ script.js
@@ -0,0 +1,13 @@
+//selecting html elements
+
+//Form
+const jobForm = document.getElementById('job-form');
+// table
+const jobList = document.getElementById('#job-list tbody');
+
+//Function to add job
+function addJob(event) {
+    event.preventDefault(); //no refreshing page when submit
+
+    //
+}
