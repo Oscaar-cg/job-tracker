d77482df71aa25f766a5088072688dbab86a1dbd
Oscaar-cg <oscar.canoa@outlook.fr>
2025-10-03 09:43:25 -0600

    Create folder for each js and add code in the analyzer.js

---
+++ js/analyzer.js
@@ -0,0 +1,11 @@
+// select form element by id
+const resumeForm = document.getElementById('resume_form');
+//select file input
+const resumeFile = document.getElementById('resume_file');
+ //select textarea
+ const resumeText = document.getElementById('resume_text');
+ // select div to show analysis result
+ const resultDetails = document.getElementById('result_details');
+
+ //add event listener when user submits form
+ resumeForm.addEventListener()
