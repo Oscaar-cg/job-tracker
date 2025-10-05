b5c0c7e8c33cb5231b8efab9d4d72018e057e514
Oscaar-cg <oscar.canoa@outlook.fr>
2025-10-05 00:50:46 -0600

    Add code to analyzer.js, read a txt and output the txt

--- js/analyzer.js
+++ js/analyzer.js
@@ -12,11 +12,39 @@ const resumeFile = document.getElementById('resume_file');
     event.preventDefault();
 
     // get pasted txt' (del extra space)
-    const pastedText = resumeText.ariaValueMax.trim();
+    const pastedText = resumeText.value.trim();
 
     //get uploaded file first (trim enleve debut fin space)
     const file = resumeFile.files && resumeFile.files[0];
 
     //clear prev result
     resultDetails.innerHTML ='';
+
+    //verifier si fichier upload
+    if(file) {
+      const reader = new FileReader(); // create un lecteur de fichier
+
+      reader.onload = function(e) {
+         const fileText = e.target.result; // texte du fichier
+         console.log(fileText); //pout tester la lecture
+      };
+      reader.readAsText(file);
+    }
+    // text for analyze
+    let resumeContent = pastedText;
+    //if uploaded file get text
+    if(file) {
+      const reader = new FileReader();
+
+      reader.onload = function(e) {
+         const fileText = e.target.result;
+         resumeContent = fileText;
+         
+         resultDetails.innerText = resumeContent;
+      };
+      
+      reader.readAsText(file);
+    } else {
+      resultDetails.innerText = resumeContent;
+    }
  });
