8a9a8fc28629740c5fa9528be8710c7973c0b0e9
Oscaar-cg <oscar.canoa@outlook.fr>
2025-10-28 08:45:12 -0600

    feat: connect Job Tracker to PHP & MySql(insert+ fetch jobs)

--- js/jobTracker.js
+++ js/jobTracker.js
@@ -1,7 +1,7 @@
 //selecting html elements
 
 //Form
-const jobForm = document.getElementById('job-form');
+const jobForm = document.getElementById('jobForm');
 // table
 const jobList = document.querySelector('#job-list tbody');
 
@@ -14,24 +14,41 @@ function addJob(event) {
     const company = document.getElementById('company').value;
     const status = document.getElementById('status').value;
 
-    //Create new table row element<tr>
-    const row = document.createElement('tr');
+    //Send data to PHP server
+    fetch("php/insert_job.php", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/x-www-form-urlencoded",
+        },
+        body: `title=${encodeURIComponent(title)}&company=${encodeURIComponent(company)}&status=${encodeURIComponent(status)}`
+    })
 
-    // fill row w/ table cell & delete button
-    row.innerHTML = `
-    <td>${title}</td>
-    <td>${company}</td>
-    <td>${status}</td>
-    <td><button class="delete-btn">Delete</button></td>
-`;
-//Add new row to table
-    jobList.appendChild(row);
+    //Server response handling
+    .then(response => response.text())
+    .then(data => {
+        alert(data);
 
-    // Reset form, user can add another job
-    jobForm.reset();
+        //Create new table row element<tr>
+        const row = document.createElement('tr');
+
+        // fill row w/ table cell & delete button
+        row.innerHTML = `
+        <td>${title}</td>
+        <td>${company}</td>
+        <td>${status}</td>
+        <td><button class="delete-btn">Delete</button></td>
+        `;
+
+        //Add new row to table
+        jobList.appendChild(row);
+
+        // Reset form, user can add another job
+        jobForm.reset();
+    })
+    .catch(error => console.error("Error :", error));
 }
 
-// Fn to  delete a job
+// Fn to delete a job
 function deleteJob(event) {
     if(event.target.classList.contains('delete-btn')) {
         const row = event.target.parentElement.parentElement;
